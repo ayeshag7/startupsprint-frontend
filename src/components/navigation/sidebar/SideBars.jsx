09cd230b471ebba1cfd3b582eb31f400ebf8efde
ayeshag7 <ayeshaiqbal2592@gmail.com>
2024-12-27 15:57:18 +0500

    Modified dashboard colors

--- src/components/navigation/sidebar/SideBars.jsx
+++ src/components/navigation/sidebar/SideBars.jsx
@@ -41,15 +41,32 @@ export default function SideBars() {
 
     return (
         <div
-            className={`flex flex-col justify-between bg-white gap-2 h-screen transition-all duration-500 ease-in-out ${isCollapsed ? 'w-20' : 'w-64'} flex-shrink-0`}
+            className={`flex flex-col justify-between bg-white gap-2 h-screen transition-all duration-500 ease-in-out ${isCollapsed ? 'w-24' : 'w-64'} flex-shrink-0 overflow-x-hidden`}
         >
+
             {/* Logo and Collapse Button */}
             <div className="flex items-center justify-between p-5">
                 <img src={mainLogo} alt="YANA Logo" height={62} width={96} className={`${isCollapsed ? 'w-0 h-0 opacity-0' : 'overflow-hidden'}`} />
                 {isCollapsed ? (
                     <img src={mainLogo} alt="Collapsed Logo" className="h-10 hover:scale-110 cursor-pointer" onClick={toggleSidebarState} />
                 ) : (
-                    <img src={leftarrow} alt="Collapse Icon" className="hover:scale-110 cursor-pointer" onClick={toggleSidebarState} />
+                    <svg
+                        className="hover:scale-110 cursor-pointer text-[#1836b2] dark:text-[#e7c94d]"
+                        width="21"
+                        height="37"
+                        viewBox="0 0 21 37"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        onClick={toggleSidebarState}
+                        >
+                        <path
+                            d="M9.625 29.2916L3.5 18.4999L9.625 7.70825M16.625 29.2916L10.5 18.4999L16.625 7.70825"
+                            stroke="currentColor"
+                            stroke-width="3"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
                 )}
             </div>
 
@@ -61,34 +78,38 @@ export default function SideBars() {
                             <NavLink
                                 to={to}
                                 className={({ isActive }) =>
-                                    `flex font-medium p-1.5 rounded-lg relative gap-4 hover:bg-[#ffe6e9] ${isActive
-                                        ? `text-red-600 bg-[#ffe6e9] ${isCollapsed
-                                            ? "before:content-[''] before:absolute before:left-1/2 before:top-1/2 before:w-9 before:h-9 before:rounded-md before:transform before:-translate-x-1/2 before:-translate-y-1/2"
-                                            : "before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[3px] before:bg-red-600 before:rounded-l-md"
+                                    `flex font-medium p-1.5 rounded-lg relative gap-4 hover:bg-[#b8e2f9] ${isActive
+                                    ? `text-[#1836b2] bg-[#b8e2f9] ${isCollapsed
+                                        ? "before:content-[''] before:absolute before:left-1/2 before:top-1/2 before:w-9 before:h-9 before:rounded-md before:transform before:-translate-x-1/2 before:-translate-y-1/2"
+                                        : "before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[3px] before:bg-[#1836b2] before:rounded-l-md"
                                         }`
-                                        : `text-gray-800`
+                                    : `text-gray-800`
                                     } ${isCollapsed ? 'justify-center rounded-full' : ''}`
                                 }
                                 onClick={hasSubmenu ? toggleCustomersSection : undefined}
-                            >
+                                >
                                 <div className="group flex relative">
-                                    <span className="flex items-center justify-center">{React.cloneElement(icon, { isActive: to === window.location.pathname })}</span>
+                                    <span className="flex items-center justify-center">
+                                    {/* Pass isActive to the icon */}
+                                    {React.cloneElement(icon, { isActive: to === window.location.pathname })}
+                                    </span>
 
                                     {isCollapsed && (
-                                        <span className="group-hover:opacity-100 transition-opacity bg-[#FFE6E9] text-sm text-[#D61125] rounded-md absolute left-full ml-5 px-2 py-1 opacity-0 z-50">
-                                            {label}
-                                        </span>
+                                    <span className="group-hover:opacity-100 transition-opacity bg-[#b8e2f9] text-sm text-[#1836b2] rounded-md absolute left-full ml-5 px-2 py-1 opacity-0 z-50">
+                                        {label}
+                                    </span>
                                     )}
                                 </div>
 
                                 {!isCollapsed && (
                                     <div className="flex items-center justify-between w-full">
-                                        <span>{label}</span>
+                                    <span>{label}</span>
 
-                                        {hasSubmenu && !isCollapsed && <ExpandableIcon isExpanded={isSubMenuExpanded} />}
+                                    {hasSubmenu && !isCollapsed && <ExpandableIcon isExpanded={isSubMenuExpanded} />}
                                     </div>
                                 )}
                             </NavLink>
+
 
                             {/* Render Submenus Dynamically */}
                             {hasSubmenu && isSubMenuExpanded && !isCollapsed && (
@@ -98,7 +119,7 @@ export default function SideBars() {
                                             <NavLink
                                                 to={subTo}
                                                 className={({ isActive }) =>
-                                                    `hover:bg-[#ffe6e9] flex items-center font-medium p-1.5 text-sm transition-colors rounded ${isActive ? 'text-red-600 bg-[#ffe6e9]' : 'text-gray-800 hover:text-red-600'
+                                                    `hover:bg-[#ffe6e9] flex items-center font-medium p-1.5 text-sm transition-colors rounded ${isActive ? 'text-[#1836b2] bg-[#ffe6e9]' : 'text-gray-800 hover:text-[#1836b2]'
                                                     }`
                                                 }
                                             >
